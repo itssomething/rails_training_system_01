5a51e2b834c0c3b0ffe13f7c187958f8976017ca
Huyen Cherry <huyencherry137@gmail.com>
2018-08-16 15:29:35 +0700

    Edit Subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,16 +2,10 @@ class Subject < ApplicationRecord
   has_many :user_subjects
   has_many :course_subjects
 
-  enum status: {active: 0, unactive: 1} 
+  enum status: {active: 0, unactive: 1}
+  enum category: {git: 0, ruby: 1, rails: 2}
 
   def get_category
-    case self.category
-    when 0
-      return I18n.t("subject_type_git")
-    when 1
-      return I18n.t("subject_type_ruby")
-    else
-      return I18n.t("subject_type_rails")
-    end
+    I18n.t("subject_type_#{self.category}")
   end
 end
