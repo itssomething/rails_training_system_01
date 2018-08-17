dfe490ae6810b8ebbce7948ad4c27585c0b9e591
Manh Vu <vdmanh107@gmail.com>
2018-08-17 11:23:02 +0700

    course crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en/ do
-    get "courses/show"
     devise_for :users
     root "static_pages#home"
+    get "courses/show"
     get "static_pages/subject"
     get "static_pages/in_subject"
     get "static_pages/report"
     get "static_pages/login"
     get "static_pages/profile"
     get "static_pages/edit_profile"
-    resources :user_courses
     get "subjects/show"
+
+    resources :user_courses
+    resources :courses
   end
 end
